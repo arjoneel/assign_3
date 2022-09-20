836ab9f523afe72d16d115c667f672b83e4bd7c6
orpit <orpit@LAPTOP-9OC8AFJ4>
2022-09-20 12:13:38 -0400

    Add project files.

---
+++ assign_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Arrey = new int[25];
+
+            int n1 = 0; int n2 = 1; int num = 0;
+
+            Arrey[0] = n1;
+            Arrey[1] = n2;
+
+            for (int i = 2; i < Arrey.Length; i++)
+            {
+                num = n1 + n2;
+
+                Arrey[i] = num;
+
+                n1 = n2;
+                n2 = num;
+            }
+
+
+            for (int i = 0; i < Arrey.Length; i++)
+            {
+                Console.WriteLine($"Fibonacci number({i}) = {Arrey[i]}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
